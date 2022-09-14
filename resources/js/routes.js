4eb985b668abd2c4449490b62409e5aa5dbc0d98
A-Hashemian <amin@hashemian.tech>
2022-09-14 21:31:55 +0300

    up_14

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,3 +2,25 @@ const Welcome = () => import('./components/Welcome.vue')
 const ProductList = () => import('./components/Product/List.vue')
 const ProducCreate = () => import('./components/Produc/Add.vue')
 const ProducEdit = () => import('./components/Produc/Edit.vue')
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Welcome
+    },
+    {
+        name: 'ProductList',
+        path: '/Products',
+        component: ProductList
+    },
+    {
+        name: 'categoryEdit',
+        path: '/Products/:id/edit',
+        component: ProducCreate
+    },
+    {
+        name: 'categoryAdd',
+        path: '/Products/add',
+        component: ProducEdit
+    }
+]
