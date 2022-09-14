c14ade5c39c3a0c6233c058c92fafcee468a4c1d
A-Hashemian <amin@hashemian.tech>
2022-09-14 19:50:38 +0300

    up_09

---
+++ resources/js/components/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>LaraCRUD</h1>
+            <a href="https://www.linkedin.com/in/amin-hashemian/" target="_blank">Amin Hashemian</a>
+        </div>
+    </div>
+</template>
