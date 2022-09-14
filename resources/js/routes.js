16ee2d1fc6f31e536998f23191c91c4352caf7ea
A-Hashemian <amin@hashemian.tech>
2022-09-14 21:29:22 +0300

    up_13

---
+++ resources/js/routes.js
@@ -0,0 +1,4 @@
+const Welcome = () => import('./components/Welcome.vue')
+const ProductList = () => import('./components/Product/List.vue')
+const ProducCreate = () => import('./components/Produc/Add.vue')
+const ProducEdit = () => import('./components/Produc/Edit.vue')
