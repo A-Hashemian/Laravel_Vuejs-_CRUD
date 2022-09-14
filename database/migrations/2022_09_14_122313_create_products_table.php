89337e1453b607156172096bf3f1ce7cbf542a0b
A-Hashemian <amin@hashemian.tech>
2022-09-14 16:48:16 +0300

    up_04

--- database/migrations/2022_09_14_122313_create_products_table.php
+++ database/migrations/2022_09_14_122313_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('picture');
-            $table->timestamps('created_at');
+            $table->timestamps();
         });
     }
 
