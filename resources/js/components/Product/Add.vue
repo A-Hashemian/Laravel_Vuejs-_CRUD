f4c5d73089946b0620bc5906639c59f0697ba61b
A-Hashemian <amin@hashemian.tech>
2022-09-14 21:15:56 +0300

    up_11

---
+++ resources/js/components/Product/Add.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Add Product</h4>
+                </div>
+                <div class="card-body">
+                    <form @submit.prevent="create">
+                        <div class="row">
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" v-model="products.name">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Picture</label>
+                                    <input type="text" class="form-control" v-model="products.picture">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"add-product",
+    data(){
+        return{
+            product:{
+                name:"",
+                picture:""
+            }
+        }
+    },
+    methods:{
+        async create(){
+            await this.axios.post('/api/products', this.products).then(response=>{
+                this.$router.push({name:"productList"})
+            }).catch(error=>{
+                console.log(error)
+            })
+        }
+    }
+}
+</script>
